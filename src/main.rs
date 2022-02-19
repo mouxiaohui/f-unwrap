8c15581fd9683dbd0b7a771f13069d1dfc6a85e8
mouxiaohui <18367652239@163.com>
2022-02-19 15:25:16 +0800

    get arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{Arg, Command};
+
+const OPT_PATH: &str = "PATH";
+
 fn main() {
-    println!("Hello, world!");
+    // 获取命令行参数
+    let matches = Command::new("f-unwrap")
+        .about("Check the use of the 'unwrap()' function in your project")
+        .arg(Arg::new(OPT_PATH).help("Folder to check").index(1).default_value("."))
+        .get_matches();
+
+    match matches.value_of(OPT_PATH) {
+        Some(path) => run(path),
+        None => run(".")
+    };
+}
+
+fn run(path: &str) {
+    println!("{}", path);
 }
